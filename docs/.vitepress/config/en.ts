6f474f5af798c091d8e10b456c82cbd897bc329f
frischler <frischler@posteo.de>
2025-02-12 18:02:41 +0100

    test for paths not having /en at the beginning

--- docs/.vitepress/config/en.ts
+++ docs/.vitepress/config/en.ts
@@ -6,21 +6,21 @@ export const en = defineConfig({
     description: "Affordable eye-tracking solutions for more inclusion",
     themeConfig: {
         nav: [
-            { text: "Get started", link: "/en/01-introduction/01-welcome" },
-            { text: "Contact", link: "/en/06-miscellaneous/contact" },
-            { text: "Donate", link: "/en/06-miscellaneous/donate" },
-            { text: "Imprint", link: "/en/06-miscellaneous/imprint" },
-            { text: "Privacy", link: "/en/06-miscellaneous/privacy" },
+            { text: "Get started", link: "/01-introduction/01-welcome" },
+            { text: "Contact", link: "/06-miscellaneous/contact" },
+            { text: "Donate", link: "/06-miscellaneous/donate" },
+            { text: "Imprint", link: "/06-miscellaneous/imprint" },
+            { text: "Privacy", link: "/06-miscellaneous/privacy" },
         ],
 
         sidebar: [
             {
                 text: "Introduction",
                 items: [
-                    { text: "Welcome", link: "/en/01-introduction/01-welcome" },
+                    { text: "Welcome", link: "/01-introduction/01-welcome" },
                     {
                         text: "What to expect",
-                        link: "/en/01-introduction/02-what-you-can-expect",
+                        link: "/01-introduction/02-what-you-can-expect",
                     },
                 ],
             },
@@ -29,11 +29,11 @@ export const en = defineConfig({
                 items: [
                     {
                         text: "ALS and Motor Neuron Diseases",
-                        link: "/en/02-basic-knowledge/01-motoneuron-diseases",
+                        link: "/02-basic-knowledge/01-motoneuron-diseases",
                     },
                     {
                         text: "Eye-Tracking: What is it?",
-                        link: "/en/02-basic-knowledge/02-eye-tracking-what-is-this",
+                        link: "/02-basic-knowledge/02-eye-tracking-what-is-this",
                     },
                 ],
             },
@@ -42,24 +42,24 @@ export const en = defineConfig({
                 items: [
                     {
                         text: "Introduction",
-                        link: "/en/03-the-eye-tracking-guide/01-introduction",
+                        link: "/03-the-eye-tracking-guide/01-introduction",
                     },
                     {
                         text: "Communication Boards",
-                        link: "/en/03-the-eye-tracking-guide/02-communication-boards",
+                        link: "/03-the-eye-tracking-guide/02-communication-boards",
                     },
                     {
                         text: "Smartphone",
-                        link: "/en/03-the-eye-tracking-guide/03-smartphone",
+                        link: "/03-the-eye-tracking-guide/03-smartphone",
                     },
-                    { text: "Webcam", link: "/en/03-the-eye-tracking-guide/04-webcam" },
+                    { text: "Webcam", link: "/03-the-eye-tracking-guide/04-webcam" },
                     {
                         text: "Eye-Tracking Glasses",
-                        link: "/en/03-the-eye-tracking-guide/05-eye-tracking-glasses",
+                        link: "/03-the-eye-tracking-guide/05-eye-tracking-glasses",
                     },
                     {
                         text: "Products",
-                        link: "/en/03-the-eye-tracking-guide/06-products",
+                        link: "/03-the-eye-tracking-guide/06-products",
                     },
                 ],
             },
@@ -68,31 +68,31 @@ export const en = defineConfig({
                 items: [
                     {
                         text: "AITrack",
-                        link: "/en/04-software-and-hardware-in-detail/aitrack",
+                        link: "/04-software-and-hardware-in-detail/aitrack",
                     },
                     {
                         text: "Beam Eye-Tracker",
-                        link: "/en/04-software-and-hardware-in-detail/beam",
+                        link: "/04-software-and-hardware-in-detail/beam",
                     },
                     {
                         text: "EyeTrackVR",
-                        link: "/en/04-software-and-hardware-in-detail/eyetrackvr",
+                        link: "/04-software-and-hardware-in-detail/eyetrackvr",
                     },
                     {
                         text: "Miranda",
-                        link: "/en/04-software-and-hardware-in-detail/miranda",
+                        link: "/04-software-and-hardware-in-detail/miranda",
                     },
                     {
                         text: "Opentrack",
-                        link: "/en/04-software-and-hardware-in-detail/opentrack",
+                        link: "/04-software-and-hardware-in-detail/opentrack",
                     },
                     {
                         text: "OptiKey",
-                        link: "/en/04-software-and-hardware-in-detail/optikey",
+                        link: "/04-software-and-hardware-in-detail/optikey",
                     },
                     {
                         text: "Pupil",
-                        link: "/en/04-software-and-hardware-in-detail/pupil",
+                        link: "/04-software-and-hardware-in-detail/pupil",
                     },
                 ],
             },
@@ -101,29 +101,29 @@ export const en = defineConfig({
                 items: [
                     {
                         text: "Companies, Games, Forums",
-                        link: "/en/05-more-eye-tracking-topics/companies-games-forums",
+                        link: "/05-more-eye-tracking-topics/companies-games-forums",
                     },
                 ],
             },
             {
                 text: "Miscellaneous",
                 items: [
-                    { text: "About Us", link: "/en/06-miscellaneous/about-us" },
-                    { text: "Contact", link: "/en/06-miscellaneous/contact" },
-                    { text: "Press", link: "/en/06-miscellaneous/press" },
-                    { text: "Donate", link: "/en/06-miscellaneous/donate" },
-                    { text: "Imprint", link: "/en/06-miscellaneous/imprint" },
-                    { text: "Privacy", link: "/en/06-miscellaneous/privacy" },
+                    { text: "About Us", link: "/06-miscellaneous/about-us" },
+                    { text: "Contact", link: "/06-miscellaneous/contact" },
+                    { text: "Press", link: "/06-miscellaneous/press" },
+                    { text: "Donate", link: "/06-miscellaneous/donate" },
+                    { text: "Imprint", link: "/06-miscellaneous/imprint" },
+                    { text: "Privacy", link: "/06-miscellaneous/privacy" },
                 ],
             },
             {
                 text: "Development & Wiki",
                 collapsed: true,
                 items: [
-                    { text: "Introduction", link: "/en/07-wiki/introduction" },
+                    { text: "Introduction", link: "/07-wiki/introduction" },
                     {
                         text: "Infrared Safety",
-                        link: "/en/07-wiki/infrared-safety",
+                        link: "/07-wiki/infrared-safety",
                     },
                 ],
             },
